998868f323f9fc6fe573b7dfdaa9e5098bd29eb3
prince12xo <333princepatel@gmail.com>
2026-06-17 23:44:31 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -16,7 +16,7 @@ app.post('/ask', async (req, res) => {
     }
 
     const genAI = new GoogleGenerativeAI(apiKey);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
     const rawSpeechInput = req.body.prompt;
 
     const pipelinePrompt = `
